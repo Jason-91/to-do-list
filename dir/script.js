4f407abe61584548e41418ebd67da3b83d889d9f
Jason-91 <hyunjoon1991@gmail.com>
2022-05-09 03:06:18 -0700

    added functionality to adding new tasks

--- dir/script.js
+++ dir/script.js
@@ -6,6 +6,9 @@ const listDisplayContainer = document.querySelector('[data-list-display-containe
 const listTitleElement = document.querySelector('[data-list-title]')
 const listCountElement = document.querySelector('[data-list-count]')
 const tasksContainer = document.querySelector('[data-tasks]')
+const taskTemplate = document.getElementById('task-template')
+const newTaskForm = document.querySelector('[data-new-task-form]')
+const newTaskInput = document.querySelector('[data-new-task-input]')
 
 const local_storage_list_key = 'task.lists'
 const local_storage_selected_list_id_key ='task.selectedListId'
@@ -35,8 +38,23 @@ newListForm.addEventListener('submit', e => {
     saveAndRender()
 })
 
+newTaskForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const taskName = newTaskInput.value
+    if (taskName == null || taskName === '') return
+    const task = createTask(taskName)
+    newTaskInput.value = null
+    const selectedList = lists.find(list => list.id === selectedListId)
+    selectedList.tasks.push(task)
+    saveAndRender()
+})
+
 function createList(name){
-    return {id: Date.now().toString(), name: name, tasks: []}
+    return {id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function createTask(name) {
+    return {id: Date.now().toString(), name: name, complete: false }    
 }
 
 function saveAndRender() {
@@ -60,7 +78,22 @@ function render() {
         listDisplayContainer.style.display = ''
         listTitleElement.innerText = selectedList.name
         renderTaskCount(selectedList)
+        clearElement(tasksContainer)
+        renderTasks(selectedList)
     }
+}
+
+function renderTasks(selectedList) {
+    selectedList.tasks.forEach(task => {
+        const taskElement = document.importNode(taskTemplate.content, true)
+        const checkbox = taskElement.querySelector('input')
+        checkbox.id = task.id
+        checkbox.checked = task.complete
+        const label = taskElement.querySelector('label')
+        label.htmlFor = task.id
+        label.append(task.name)
+        tasksContainer.appendChild(taskElement)
+    })
 }
 
 function renderTaskCount(selectedList) {
